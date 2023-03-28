f0b49a962c8aa5e86dc9f717bc50981e5c00045d
Dina <dinagiacobbi@gmail.com>
2023-03-28 13:18:09 -0800

    Adding product image tags

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
 let main = document.querySelector('.main')
 
 function renderProducts() {
-  let productCard = document.querySelector('.product-card')
-  main.append(productCard)
-
-  let productList = document.querySelector('.product-list')
-  productCard.append(productList)
-
   for (let i = 0; i < data.length; i += 1) {
+    let productCard = document.querySelector('.product-card')
+    main.append(productCard)
+
+    let productList = document.querySelector('.product-list')
+    productCard.append(productList)
+
     let itemsList = document.createElement('li')
-    itemsList.innerHTML = `Item: ${data[i].title}<br>
-   Price each: ${data[i].price}
-   ${data[i].image}
-   `
-
-
-
-
-
+    itemsList.innerHTML += `Item: ${data[i].title}<br>
+     Price each: ${data[i].price} <br>
+     <img src=${data[i].image}>`
 
     productCard.append(itemsList)
   }
-
-  productCard.innerHTML += `<p>Item 1 is: ${data[0].title}</p>`
 }
 
 let productsBtn = document.querySelector('#display-button')
