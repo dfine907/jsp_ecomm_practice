69a38f7fc9ab36fe92192b2450f4d5364cb1bda1
Dina <dinagiacobbi@gmail.com>
2023-04-07 09:11:54 -0800

    Refactoring to using API early stages

--- app.js
+++ app.js
@@ -3,44 +3,68 @@ const totalDiv = document.querySelector('.total')
 
 let grandTotal = 0
 
-function renderProducts() {
-  for (let i = 0; i < data.length; i += 1) {
-    let card = document.createElement('div')
-    card.classList.add('product-card')
-    main.append(card)
+// function renderProducts() {
+//   for (let i = 0; i < data.length; i += 1) {
+//     let card = document.createElement('div')
+//     card.classList.add('product-card')
+//     main.append(card)
 
-    let itemTitle = document.createElement('p')
-    itemTitle.classList.add('list-items')
-    itemTitle.innerHTML += ` ${data[i].title}`
-    card.append(itemTitle)
+//     let itemTitle = document.createElement('p')
+//     itemTitle.classList.add('list-items')
+//     itemTitle.innerHTML += ` ${data[i].title}`
+//     card.append(itemTitle)
 
-    let itemPrice = document.createElement('p')
-    itemPrice.classList.add('list-items')
-    itemPrice.innerHTML += ` ${data[i].price}`
-    card.append(itemPrice)
+//     let itemPrice = document.createElement('p')
+//     itemPrice.classList.add('list-items')
+//     itemPrice.innerHTML += ` ${data[i].price}`
+//     card.append(itemPrice)
 
-    let itemImg = document.createElement('img')
-    itemImg.classList.add('list-items')
-    itemImg.src = `${data[i].image}`
-    card.append(itemImg)
+//     let itemImg = document.createElement('img')
+//     itemImg.classList.add('list-items')
+//     itemImg.src = `${data[i].image}`
+//     card.append(itemImg)
 
-    let buyButton = document.createElement('button')
-    buyButton.innerHTML = 'BUY'
-    buyButton.classList.add('buy-btn')
+//     let buyButton = document.createElement('button')
+//     buyButton.innerHTML = 'BUY'
+//     buyButton.classList.add('buy-btn')
 
-    buyButton.setAttribute('price', ` ${data[i].price}`)
+//     buyButton.setAttribute('price', ` ${data[i].price}`)
     
-    const addTotals = (price) => {
-      grandTotal += price
-      totalDiv.innerHTML = `Total: $ ${grandTotal}`
-    }
+//     const addTotals = (price) => {
+//       grandTotal += price
+//       totalDiv.innerHTML = `Total: $ ${grandTotal}`
+//     }
 
-    buyButton.addEventListener('click', function () {
-      console.log('Works')
-      addTotals(Number((data[i].price).toFixed(2)))
+//     buyButton.addEventListener('click', function () {
+//       console.log('Works')
+//       addTotals(Number((data[i].price).toFixed(2)))
+//     })
+
+//     card.append(buyButton)
+//   }
+// }
+
+// renderProducts()
+
+// ****  WITH API ******
+
+
+const URL_API = "https://fakestoreapi.com/products"
+
+function getData(url) {
+  fetch(url)
+  .then(response => {
+    return response.json()
+  }).then(data => {
+    data.forEach(product => {
+      const display = document.createElement('div')
+      display.classList.add('display-card')
+      display.innerHTML += `${product.title}`
+      main.append(display)
     })
+  })
 
-    card.append(buyButton)
-  }
+
 }
-renderProducts()
+
+getData(URL_API)
